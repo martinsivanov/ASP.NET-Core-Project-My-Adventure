bac571d7b427d98be3096a3e16fe7f99250e6a02
martinsivanov <martingivanov108@gmail.com>
2024-06-25 21:16:10 +0300

    Added test 1

--- MyAdventure/Startup.cs
+++ MyAdventure/Startup.cs
@@ -81,6 +81,7 @@ namespace MyAdventure
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
             });
+            //Test 1
         }
     }
 }
